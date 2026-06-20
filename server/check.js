d4c66e0292c719207dfa549e45eb27431a05a6cd
Ajithravii18 <ajithravii28@gmail.com>
2026-06-20 17:07:21 +0530

    Delete server directory

--- server/check.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI).then(async () => {
-  const Match = require('./models/Match');
-  const m = await Match.find({ $or: [{homeTeam: 'France'}, {awayTeam: 'France'}] });
-  console.log(m.map(x => `${x.homeTeam} ${x.homeScore} - ${x.awayScore} ${x.awayTeam} | Winner: ${x.winner}`));
-  process.exit(0);
-});
